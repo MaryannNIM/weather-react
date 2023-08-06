62243fe7e54fbc2687256e07c68a7b6ed227f156
Maryann <Onunakum@gmail.com>
2023-08-06 22:50:38 +0200

    Added forecast for a day

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -17,7 +17,7 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           <div className="clearfix">
             <div className="float-left"></div>
-            <WeatherIcon code={props.data.icon} />
+            <WeatherIcon code={props.data.icon} size={52} />
 
             <div className="float-left">
               <WeatherTemperature celsius={props.data.temperature} />
